9f7ac09786e4e49501de3334f41a3ad73f41b476
ZeengFong <163157869+ZeengFong@users.noreply.github.com>
2026-02-14 16:27:12 -0700

    Created Swift Project

---
+++ Swift/LockIn/LockIn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockIn
+//
+//  Created by Luca Suanez on 2026-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/LockIn/LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by Luca Suanez on 2026-02-14.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
